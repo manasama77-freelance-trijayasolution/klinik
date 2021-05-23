834c941aa335ea0dc23b9da6e666636da5164100
Adam PM <adam.pm77@gmail.com>
2021-05-23 14:53:06 +0700

    update gitignore

--- design/koneksi/file.php
+++
@@ -1,9 +0,0 @@
-	<?php
-	$host		="localhost";
-	$user		="u1029257_masterweb";
-	$pass		="M355age45";
-	$database	="u1029257_kyoaims";
-
-	$con		=mysqli_connect($host, $user, $pass, $database);
-	
-	?>
--- design/koneksi/xxx.php
+++
@@ -1,4 +0,0 @@
-<script>
-if (!window.console) console = {};
-			console.info = console.info("ready to serve, sir...") || function(){};
-</script>
